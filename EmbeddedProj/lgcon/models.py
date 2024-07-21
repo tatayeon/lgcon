2e1791b718358b6b90a478c77a089637210b2dc0
taetae <dmsqls0409@gmail.com>
2024-07-21 18:26:45 +0900

    이상함

--- EmbeddedProj/lgcon/models.py
+++ EmbeddedProj/lgcon/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import os
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30, unique=True)
+
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.category_name
+    
+    def get_absolute_url(self):
+        return f'/lgcom/category/{self}/'
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=20)
+
+    price = models.IntegerField()
+
+    menu_image = models.ImageField(upload_to='lgcon/menu_image/', blank= True)
+
+    category = models.ForeignKey(Category, null=True, on_delete = models.SET_NULL)
+
+
+
