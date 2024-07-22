213f08b235611a008fcb8bbae9e1d78591bf7762
taetae <dmsqls0409@gmail.com>
2024-07-22 16:55:55 +0900

    Order모델 생성 slug변동

--- EmbeddedProj/lgcon/models.py
+++ EmbeddedProj/lgcon/models.py
@@ -1,31 +1,42 @@
 from django.db import models
-import os
-from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=30, unique=True)
-
-    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
 
     def __str__(self):
         return self.category_name
     
     def get_absolute_url(self):
-        return f'/lgcon/category/{self}/'
+        return f'/lgcon/category/{self.slug}/'
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.category_name, allow_unicode=True)
+        super().save(*args, **kwargs)
     
     class Meta:
-        verbose_name_plural = "Categories"
-
+        verbose_name_plural = "categories"
 
 class Menu(models.Model):
     name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    menu_image = models.ImageField(upload_to='lgcon/menu_image/', blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
+
+    def __str__(self):
+        return f"[{', '.join(category.category_name for category in self.categories.all())}] {self.name}"
+
+
+class Order(models.Model):
+    Order_at = models.DateTimeField(auto_now=True)
+
+    order_name = models.CharField(max_length=30)
 
     price = models.IntegerField()
 
-    menu_image = models.ImageField(upload_to='lgcon/menu_image/', blank= True)
+    quantity = models.IntegerField()
 
-    category = models.ForeignKey(Category, null=True, on_delete = models.SET_NULL)
-
-
+    bell_number = models.IntegerField()
 
