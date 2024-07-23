b2c0b2a80d01a2a81e12ef66d47e5eae7ab9aa31
jooon01 <rlawnsgk0140@naver.com>
2024-07-24 03:07:35 +0900

    제거

--- EmbeddedProj/EmbeddedProj/settings.py
+++ EmbeddedProj/EmbeddedProj/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'skrr',
         'USER': 'root',
-        'PASSWORD': 'ekdma135!', 
+        'PASSWORD': '', 
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
